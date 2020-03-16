8b387ee0ddaa8351e8f25703ac0603bb7bde1582
Hugo <hugo_math@etu.u-bourgogne.fr>
2020-03-16 18:28:02 +0100

    ts french for objects

--- image.py
+++ image.py
@@ -119,10 +119,12 @@ class Image:
 
     def detectOther(self):
         self.limitConfidence = 0.3
-        self.objects = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor"]
+        """    self.objects = ["background", "aeroplane", "bicycle", "bird", "boat",
+	    "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	    "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	    "sofa", "train", "tvmonitor"]
+        """
+        self.objects = ["arriere-plan", "avion", "velo", "oiseau", "bateau", "bouteille", "autobus", "voiture", "chat", "chaise", "vache", "table","chien", "cheval", "moto", "personne", "plante en pot", "mouton", "sofa", "train", "moniteur"]
         self.colors = np.random.uniform(0, 255, size=(len(self.objects), 3))
         self.net = cv2.dnn.readNetFromCaffe(path_prototxt, path_model)
 
